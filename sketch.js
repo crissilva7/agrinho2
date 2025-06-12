37d411b1ac790983aaf09dd4a248c3bb43e21624
crissilva7 <cristiane.silva.barbosa@escola.pr.gov.br>
2025-06-12 10:50:00 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,4 @@
 function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-function setup() {
   createCanvas(800, 400);
 }
 
@@ -12,7 +6,7 @@ function draw() {
   let t = constrain(map(mouseX, 0, width, 0, 1), 0, 1);
   
   // Céu
-  let skyColor = lerpColor(color(135, 206, 235), color(180, 180, 180), t); // Azul para preto
+  let skyColor = lerpColor(color(135, 206, 235), color(100, 180, 180), t); // Azul para cinza
   background(skyColor);
 
   drawGround(t);
@@ -22,7 +16,7 @@ function draw() {
 }
 
 function drawGround(t) {
-  let groundColor = lerpColor(color(50, 200, 70), color(120), t);
+  let groundColor = lerpColor(color(50, 100, 70), color(120), t);
   fill(groundColor);
   noStroke();
   rect(0, height * 0.7, width, height * 0.3);
@@ -32,11 +26,11 @@ function drawNature(t) {
   // Árvore (aparece no lado esquerdo, desaparece com t)
   push();
   let treeX = width * 0.25;
-  let treeAlpha = map(1 - t, 0, 1, 0, 255);
-  fill(139, 69, 19, treeAlpha);
-  rect(treeX, 220, 20, 80); // Tronco
-  fill(34, 139, 34, treeAlpha);
-  ellipse(treeX + 10, 200, 80, 80); // Copa
+  let treeAlpha = map(1 - t, 0, 1, 0, 215);
+  fill(200, 59, 19, treeAlpha);
+  rect(treeX, 200, 20, 80); // Tronco
+  fill(34, 109, 24, treeAlpha);
+  ellipse(treeX + 10, 200, 100, 100); // Copa
   pop();
 }
 
@@ -47,7 +41,7 @@ function drawCity(t) {
 
   for (let i = 0; i < maxBuildings; i++) {
     let x = width * 0.5 + i * 60;
-    let h = 100 + i * 20;
+    let h = 270 + i * 20;
     let alpha = map(t, 0, 1, 0, 255);
 
     rect(x, height * 0.7 - h, 40, h);
@@ -60,11 +54,13 @@ function drawMessage(t) {
   textAlign(CENTER);
   
   if (t < 0.3) {
-    text("Campo: natureza, vida simples e verde escuro", width / 2, 30);
+    text("Campo: natureza, vida simples", width / 2, 30);
   } else if (t < 0.7) {
     text("Transição: do campo para a cidade", width / 2, 30);
   } else {
-    text("Cidade: modernidade, desafios e oportunidades", width / 2, 30);
+    text("Cidade: modernidade, predios, grande pupulação, desafios", width / 2, 20);
   }
 }
+
+
 
